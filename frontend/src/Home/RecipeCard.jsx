083677a09ceb12e168f2d7cf705f904f842a4f73
Aslamvs1 <118463532+Aslamvs1@users.noreply.github.com>
2025-03-10 12:49:34 +0530

    Update RecipeCard.jsx

--- frontend/src/Home/RecipeCard.jsx
+++ frontend/src/Home/RecipeCard.jsx
@@ -8,7 +8,7 @@ function RecipeCard() {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:3000/recipes/${id}`)
+      .get(`https://recipe-full.onrender.com/recipes/${id}`)
       .then((response) => {
         console.log("Fetched data:", response.data);
         setRecipe(response.data);
